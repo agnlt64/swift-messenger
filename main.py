e336757d3b836737239c64f66b868f3626249d50
Antonin <jaimelesfraisesbleues@gmail.com>
2023-03-18 15:48:47 +0100

    expose tyo network

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from app import create_app
+import sys
 
 if __name__ == '__main__':
     app, socketio = create_app()
-    socketio.run(app, debug=True, host='127.0.0.1', port='8080')
+    if len(sys.argv) > 2:
+        if sys.argv[1] == '--expose' or sys.argv[1] == '-E':
+            socketio.run(app, debug=True, host='0.0.0.0', port=8080)
+    else:
+        socketio.run(app, debug=True, host='127.0.0.1', port=8080)
