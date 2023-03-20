08d0f6d85f95b8f174d24469201a376d04f8469b
Antonin <jaimelesfraisesbleues@gmail.com>
2023-03-20 22:45:35 +0100

    fix all bugs in css

--- app/auth.py
+++ app/auth.py
@@ -43,7 +43,7 @@ def sign_up():
             new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.chat'))
     return render_template('signup.html')
