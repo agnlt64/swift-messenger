736b25e6139461eb8994a0d786efb710ac110f20
Antonin <jaimelesfraisesbleues@gmail.com>
2023-04-03 22:10:12 +0200

    new requirements, make database remote

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from flask_socketio import SocketIO, send
 from flask_login import LoginManager
+import os
 from os import path
 import secrets
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+load_dotenv('.config')
 
 db = SQLAlchemy()
 DB_NAME = 'database.sqlite3'
@@ -21,17 +25,15 @@ def create_app():
     from .models import User, ChatGroup
     
     app.config['SECRET_KEY'] = secrets.token_urlsafe(40)
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     db.init_app(app)
-
-    with app.app_context():
-        if not path.exists(f'instance/{DB_NAME}'):
-        # delete the database each time a new table is added
-            db.create_all()
         
     login_manager = LoginManager(app)
     login_manager.login_view = 'auth.login'
     login_manager.login_message_category = 'error'
+
+    with app.app_context():
+        db.create_all()
 
     @login_manager.user_loader
     def load_user(id: int):
