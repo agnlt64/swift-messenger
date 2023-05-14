45ddcf76ab88681f0c21d41542f754c33a834863
Antonin GENELOT <Antonin@Antonin>
2023-05-14 13:25:04 +0200

    fix config file read

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ def create_app():
     app.config['SECRET_KEY'] = secrets.token_urlsafe(40)
     try:
         with open('.config', 'r') as config_file:
-            app.config['SQLALCHEMY_DATABASE_URI'] = config_file.read()
+            app.config['SQLALCHEMY_DATABASE_URI'] = config_file.read().split('\n')[0]
     except FileNotFoundError:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     db.init_app(app)
