ef7efedb1eb0c9e9f3d369a032b07a16b9c294cb
ABFStudio <jaimelesfraisesbleues@gmail.com>
2023-07-05 15:52:48 +0200

    fix prod bug

--- app/__init__.py
+++ app/__init__.py
@@ -13,11 +13,13 @@ def create_app():
     from .server.models import User, ChatGroup, Message, Task
     
     app.config['SECRET_KEY'] = secrets.token_urlsafe(40)
-    try:
-        with open('.config', 'r') as config_file:
-            app.config['SQLALCHEMY_DATABASE_URI'] = config_file.read().split('\n')[0]
-    except FileNotFoundError:
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    # Render error, fix later
+    # try:
+    #     with open('.config', 'r') as config_file:
+    #         app.config['SQLALCHEMY_DATABASE_URI'] = config_file.read().split('\n')[0]
+    # except FileNotFoundError:
+    #     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     db.init_app(app)
 
     login_manager = LoginManager(app)
