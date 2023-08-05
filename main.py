5c377ad050d0bce84b3f4ae53c02c729b22e7ff7
Antonin GENELOT <Antonin@Antonin>
2023-08-05 22:30:44 +0200

    new look for flashed messages + delete media/close.png

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     logger.info('App instance created')
     if len(sys.argv) > 2:
         if sys.argv[1] == '--expose' or sys.argv[1] == '-E':
-            socketio.run(app, debug=True, host='0.0.0.0', port=8080)
             logger.info('App is running')
+            socketio.run(app, debug=True, host='0.0.0.0', port=8080)
     else:
-        socketio.run(app, debug=True, host='127.0.0.1', port=8080)
         logger.info('App is running')
+        socketio.run(app, debug=True, host='127.0.0.1', port=8080)
