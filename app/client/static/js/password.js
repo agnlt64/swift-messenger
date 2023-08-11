d3c3b167b87fce2796edddd303355fc42cb8845a
Antonin GENELOT <Antonin@Antonin>
2023-08-11 02:16:02 +0200

    add client-side password verification

--- app/client/static/js/password.js
+++ app/client/static/js/password.js
@@ -1,7 +1,19 @@
+
+function matchRules(password) {
+    return (
+        password.length > 10 &&
+        // an uppercase letter
+        /[A-Z]/.test(password) &&
+        // a number
+        /\d/.test(password)
+    )
+}
+
 const passwordObserver = new MutationObserver(mutationList => {
     for (const mutation of mutationList) {
         if (mutation.type === 'childList') {
             try {
+                // show the passwords
                 const showPassword = document.querySelectorAll('.password i')
                 let showed = false
                 showPassword.forEach(icon => icon.addEventListener('click', () => {
@@ -17,6 +29,15 @@ const passwordObserver = new MutationObserver(mutationList => {
                         icon.classList = 'bx bxs-show'
                         icon.parentElement.children[0].type = 'password'
                     }
+                }))
+
+                // check if the passwords match the rules
+                const passwords = document.querySelectorAll('input[type=password]')
+                const submit = document.querySelector('button[type=submit]')
+                const rules = document.querySelector('.password-rules')
+                passwords.forEach(password => password.addEventListener('input', () => {
+                    submit.toggleAttribute('disabled', !matchRules(password.value))
+                    rules.classList.toggle('match', matchRules(password.value))
                 }))
             }
             catch (error) {
